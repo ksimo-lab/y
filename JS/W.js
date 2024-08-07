d4dce62359e91adc5690291f9e8c7b2643cf55d4
ksimo-lab <kenijoshua1@gmail.com>
2024-08-07 13:46:22 +0200

    Add files via upload

---
+++ JS/W.js
@@ -0,0 +1,37 @@
+let menu = document.querySelector('.menu-icon');
+// 144 
+let navbar = document.querySelector('.menu');
+
+
+menu.onclick = () =>{
+    menu.classList.toggle('move');
+    navbar.classList.toggle('active');
+
+}
+// swiper 
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay:{
+        delay:3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
---
+++ JS/dons.js
@@ -0,0 +1,40 @@
+const chartData = {
+  labels: ["Jeux chrétiens", "Animations chrétiennes", "Recruter des équipiers", "Lieux chrétiens( camps, 'pubs chrétiens',...)",],
+  data: [30, 30, 20, 20],
+};
+
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programming-stats .details ul");
+
+new Chart(myChart, {
+  type: "doughnut",
+  data: {
+    labels: chartData.labels,
+    datasets: [
+      {
+        label:"FISH DONUT",
+        data: chartData.data,
+      },
+    ],
+  },
+  options: {
+    borderWidth: 10,
+    borderRadius: 2,
+    hoverBorderWidth: 0,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+const populateUl = () => {
+  chartData.labels.forEach((l, i) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+    ul.appendChild(li);
+  });
+};
+
+populateUl();
